b977b3177a094facc6b0076733da5c788e7d2c97
Alex Abatov <a.aliksant315@gmail.com>
2025-02-16 05:28:33 +0900

    bot detection applied

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 
 import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
 dotenv.config();
 
 const app = express();
@@ -15,6 +16,41 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet()); // helmet is security middleware
 app.use(morgan("dev")); // console log to the terminal the requests
+
+// apply rate-limit to all routes
+
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1, //make sure that each request uses 1 token
+    });
+
+    if (decision.isDenied()) {
+      if (desicion.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too many requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
+    }
+
+    //check for advanced bots
+    if (
+      decision.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed()
+      )
+    ) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+    next();
+  } catch (error) {
+    console.log("Arcjet error", error);
+    next(error);
+  }
+});
 
 app.use("/api/products", productRoutes);
 
