8fa09f5cd862515f7304d269b299d6d5e5cd2e5a
Alex Abatov <a.aliksant315@gmail.com>
2025-02-17 11:41:41 +0900

    fixed navbar & theme color option

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,34 @@
+export const THEMES = [
+  {
+    name: "retro",
+    label: "Retro",
+    colors: ["#e2d5bc", "#ef9995", "#a4cbb4"],
+  },
+  {
+    name: "coffee",
+    label: "Coffee",
+    colors: ["#20161F", "#A67C58", "#807666"],
+  },
+  {
+    name: "forest",
+    label: "Forest",
+    colors: ["#171212", "#2B4C3F", "#6BAA75"],
+  },
+
+  {
+    name: "luxury",
+    label: "Luxury",
+    colors: ["#171618", "#B6862D", "#E2C697"],
+  },
+
+  {
+    name: "night",
+    label: "Night",
+    colors: ["#0F172A", "#334155", "#64748B"],
+  },
+  {
+    name: "dracula",
+    label: "Dracula",
+    colors: ["#282A36", "#BD93F9", "#FF79C6"],
+  },
+];
